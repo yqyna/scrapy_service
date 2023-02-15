bd750ef597eb154b327f38d8d532cd52bb7d3de8
yuhaiping <yuhaiping221956@ehousechina.com>
2023-02-15 11:26:50 +0800

    ADD main.py

---
+++ qsw/main.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# @FileName  :main.py
+# @Time      :2023/2/15 11:21
+# @Author    : yuhaiping
+
+from scrapy.cmdline import execute
+if __name__ == '__main__':
+    execute(['scrapy', 'crawl', 'qsw'])
